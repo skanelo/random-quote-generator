d663a2007b6c696fc737430f05e557a3fc6287e1
Stefanos <skanelo.ai@gmail.com>
2023-10-20 22:13:00 +0200

    Add random_quote_generator and tests.

---
+++ random_quote_generator/get_quote.py
@@ -0,0 +1,14 @@
+"""Module for getting random quote from database of programming quotes."""
+
+import random
+
+from random_quote_generator.quotes import quotes
+
+
+def get_quote() -> dict:
+    """Get random quote from quotes.
+
+    Returns:
+        dict: Random quote from quotes.
+    """
+    return quotes[random.randint(0, len(quotes) - 1)]
---
+++ tests/test_get_quote.py
@@ -0,0 +1,17 @@
+"""This file contains the tests for the quote module."""
+
+from random_quote_generator import get_quote
+from random_quote_generator.quotes import quotes
+
+
+def test_get_quote() -> None:
+    """Test get_quote function.
+
+    GIVEN (nothing)
+    WHEN get_quote is called
+    THEN random quote from quotes is returned
+    """
+    quote = get_quote()
+
+    assert quote in quotes
+    assert quote in quotes
